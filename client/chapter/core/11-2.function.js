4f6854499d6bad5947be70a2d6c5f12a0d77cb7d
wnsrl7250 <wnsrl7250@naver.com>
2024-11-06 16:04:02 +0900

    learn. core javascript
     - 콜백 함수

--- client/chapter/core/11-2.function.js
+++ client/chapter/core/11-2.function.js
@@ -215,3 +215,29 @@ const MASTER = (function (g) {
     },
   };
 })(window);
+
+const css = (function () {
+  function setCss(node, prop, value) {
+    if (typeof node === 'string') node = document.querySelector(node);
+    if (!(prop in document.body.style))
+      throw new ReferenceError('setCss 함수의 두 번째 인수는 유효한 css 속성 이어야 합니다.');
+
+    if (!value) throw new Error('setCss 함수의 세 번째 인수는 필수 입력 값 입니다.');
+    node.style[prop] = value;
+  }
+  function getCss(node, prop) {
+    if (typeof node === 'string') {
+      node = document.querySelector(node);
+    }
+    if (!(prop in document.body.style)) {
+      throw new ReferenceError('getCss 함수의 두 번째 인수는 유효한 css 속성 이어야 합니다.');
+    }
+    return getComputedStyle(node)[prop];
+  }
+  function css(node, prop, value) {
+    return !value ? getCss(node, prop) : setCss(node, prop, value);
+  }
+  return css;
+})();
+
+css('.first', 'color', 'red');
