5a8ea3a4314b7ddba3d4fbf6007b7008e29caf35
wnsrl7250 <wnsrl7250@naver.com>
2024-11-12 16:26:12 +0900

    learn . core javascript
    - optional
    - primitive method
    - number

--- client/chapter/core/14-2.closure.js
+++ client/chapter/core/14-2.closure.js
@@ -28,3 +28,30 @@ const handleClick = (() => {
 })();
 
 button.addEventListener('click', handleClick);
+// 개발자는 이벤트를 설정하면 그거에 따른 책임을 가져야 한다.
+
+// button.removeEventListener('click',handleClick);
+
+function useState(init) {
+  let value = init;
+
+  function read() {
+    return value;
+  }
+
+  function write(newValue) {
+    value = newValue;
+  }
+
+  return [read, write];
+}
+
+const [number, setNumber] = useState(10);
+
+/* 
+전역오염을 시키지 않고
+외부변수에 접근할 수 있는 encapsulation 함수
+함수 내부에 전달된 변수의 값을 가지고 있기 때문에 return으로 내보낼 수 있다.
+GC 수집 대상이 되지 않는다.
+지워지지 않는 렉시컬 환경을 많이 생성하기 때문에 콜스텍에 부하를 줄 수 있다. (체감 어렵다) 
+*/
