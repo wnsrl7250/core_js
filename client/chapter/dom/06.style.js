6063e648b5975be57406c3e505fd2944a530d1f4
wnsrl7250 <wnsrl7250@naver.com>
2024-11-15 15:58:00 +0900

    learn. core javascript
    - event

--- client/chapter/dom/06.style.js
+++ client/chapter/dom/06.style.js
@@ -9,6 +9,23 @@ const nav = getNode('nav');
 // - className – 클래스 전체를 문자열 형태로 반환해주는 프로퍼티로 클래스 전체를 관리할 때 유용
 // - classList – 클래스 하나를 관리할 수 있게 해주는 메서드로 개별 클래스를 조작할 때 유용
 
+console.log(nav.className); // getter
+
+nav.className = 'navigation'; // setter
+
+console.log(nav.classList);
+
+// nav.classList.add('hello');
+// nav.classList.remove('hello');
+
+console.log(nav.classList.contains('navigation'));
+
+nav.classList.toggle('is-active');
+
+// addClass(nav, 'a,b,c,d');
+// addClass(nav, ['a', 'b', 'c']);
+// addClass(nav, { first: 'a', second: 'b' }); // 값만 클래스로 들어갈 수 있게
+
 /* 스타일 변경 방법 --------------------------------------------------------- */
 
 // - style.cssText - "style" 속성 전체에 대응하므로 스타일 전체에 대한 문자열 저장
