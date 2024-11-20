c6609ddd9f5f46e843278655cfc8daef1d6b92f2
wnsrl7250 <wnsrl7250@naver.com>
2024-11-20 09:37:06 +0900

    learn. core javascript
    - calculator
       - delegation

--- client/main.js
+++ client/main.js
@@ -1,38 +1,69 @@
-// named export     => import {  } from '..'
-// default export   => import .... from '..'
+// named export           =>  import { getNode as $, insertLast } from '..'
+// default export         =>  import ... from '..'
 
 // import { getNode as $, getNodes } from './lib/dom/getNode.js';
 // import { insertLast } from './lib/dom/insert.js';
-// import clearContents from './lib/dom/clearContents.js';
 
-import {
-  getNode as $,
-  getNodes,
-  insertLast,
-  clearContents,
-  typeError,
-  refError,
-} from './lib/index.js';
+// import clearContents from "./lib/dom/clearContents.js";
 
-// 1. input 선택하기
-// 2. input 이벤트 바인딩
-// 3. input의 value 값 가져오기
-// 4. 숫자 더하기
-// 5. result에 출력하기
+import { getNode as $, getNodes, typeError, insertLast, clearContents } from './lib/index.js';
 
-const first = $('#firstNumber');
-const second = $('#secondNumber');
-const result = $('.result');
+function phase1() {
+  // 1. input 선택하기
+  // 2. input 이벤트 바인딩
+  // 3. input의 value 값 가져오기
+  // 4. 숫자 더하기
+  // 5. result에 출력하기
 
-function handleInput() {
-  const firstValue = Number(first.value);
-  const secondValue = +second.value;
-  const total = firstValue + secondValue;
+  const first = $('#firstNumber');
+  const second = $('#secondNumber');
+  const result = $('.result');
+  const clear = $('#clear');
 
-  clearContents(result);
+  function handleInput() {
+    const firstValue = Number(first.value);
+    const secondValue = +second.value;
+    const total = firstValue + secondValue;
 
-  insertLast(result, total);
+    clearContents(result);
+    insertLast(result, total);
+  }
+
+  function handleClear(e) {
+    e.preventDefault();
+
+    clearContents(first);
+    clearContents(second);
+    result.textContent = '-';
+  }
+
+  first.addEventListener('input', handleInput);
+  second.addEventListener('input', handleInput);
+  clear.addEventListener('click', handleClear);
 }
 
-first.addEventListener('input', handleInput);
-second.addEventListener('input', handleInput);
+function phase2() {
+  const calculator = $('.calculator');
+  const result = $('.result');
+  const clear = $('#clear');
+  const numberInputs = [...document.querySelectorAll('input:not(#clear)')];
+
+  function handleInput() {
+    const total = numberInputs.reduce((acc, cur) => acc + Number(cur.value), 0);
+
+    clearContents(result);
+    insertLast(result, total);
+  }
+
+  function handleClear(e) {
+    e.preventDefault();
+
+    numberInputs.forEach(clearContents);
+    result.textContent = '-';
+  }
+
+  calculator.addEventListener('input', handleInput);
+  clear.addEventListener('click', handleClear);
+}
+
+phase2();
