69ebced498ed0df419f66e6bdc1540f9a2cd2bf0
wnsrl7250 <wnsrl7250@naver.com>
2024-11-26 14:04:56 +0900

    learn. web component
    - slot
    - style

--- client/main.js
+++ client/main.js
@@ -1,10 +1,25 @@
+// console.log(temp.content.cloneNode(true));
+
 class MyElement extends HTMLElement {
   connectedCallback() {
     this.attachShadow({ mode: 'open' });
 
     this.shadowRoot.innerHTML = `
-     <div>hello</div>
-     `;
+   
+      <style>
+       @import url('./style.css'); 
+      </style>
+
+      
+      <div class="card">
+        <slot name="title">Default Title</slot>
+        <slot name="content">Default Contents</slot>
+        <slot></slot>
+      </div>
+      
+    
+    `;
   }
 }
+
 customElements.define('my-element', MyElement);
