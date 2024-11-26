a716b66a1fd21b9107e82f94d205ece5af94f229
wnsrl7250 <wnsrl7250@naver.com>
2024-11-26 21:59:47 +0900

    learn. web component

--- client/main.js
+++ client/main.js
@@ -1,24 +1,49 @@
 // console.log(temp.content.cloneNode(true));
 
 class MyElement extends HTMLElement {
-  connectedCallback() {
+  constructor() {
+    super();
     this.attachShadow({ mode: 'open' });
+    this.count = 0;
+  }
 
+  connectedCallback() {
+    this.랜더링();
+    this.card = this.shadowRoot.querySelector('.card');
+    this.card.addEventListener('click', () => {
+      this.이벤트();
+    });
+    this.card.addEventListener('click', this.이벤트.bind(this));
+  }
+
+  이벤트() {
+    console.log(++this.count);
+  }
+
+  랜더링() {
     this.shadowRoot.innerHTML = `
-   
       <style>
-       @import url('./style.css'); 
+         .card{
+          background-color:orange;
+        }
       </style>
-
-      
       <div class="card">
-        <slot name="title">Default Title</slot>
-        <slot name="content">Default Contents</slot>
-        <slot></slot>
+        <h2>Card Title</h2>
+        <slot name="me"></slot>
+        <p> description..</p>
       </div>
-      
-    
     `;
+  }
+
+  static get observedAttributes() {
+    return ['data-name'];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === 'data-name') {
+      this.랜더링();
+      console.log('리랜더링');
+    }
   }
 }
 
