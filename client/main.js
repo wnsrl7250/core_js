73b1641e33d03e1fdd96971d6e0a4393a9b279aa
wnsrl7250 <wnsrl7250@naver.com>
2024-11-26 09:01:42 +0900

    learn. storage
    - review

--- client/main.js
+++ client/main.js
@@ -37,3 +37,56 @@ textField.addEventListener('input', handleInput);
 clearButton.addEventListener('click', handleClear);
 
 init();
+
+//
+//
+//
+//
+//
+
+// // 비동기 통신을 하는 이유?
+// // - 서버에 있는 데이터를 가져오기 위해 왜?
+// // - 포켓몬 정보를 보여주는 앱 서비스를
+// // - 포켓몬 정보(?) => 포켓몬 api 주소 요청
+
+// // 포켓몬 정보를 가져왔음 2s  Promise  -> async await
+
+// // 가져온 정보를 화면에 랜더링
+
+// // async : [무 조 건] 이행된 Promise 객체를 반환
+// // await - result를 꺼낼 수 있음.
+// //       ㄴ 코드 실행 흐름 제어
+
+// const defaultOptions = {
+//   headers:{
+//     '.,..':'...'
+//   },
+//   body:null
+
+// }
+
+//   async function getData (options) {
+
+//     const {url,...rest} = {...defaultOptions , ...options};
+
+//     // fetch를 사용하면 Promise 객체가 리턴된다 -> result는 response 객체
+//     const 응답 = await fetch(url,rest);
+
+//     // 응답.json() => Promise 객체가 리턴된다 -> result Object
+//     const data = await 응답.json()
+
+//     return data
+//   }
+
+//   const monster = await getData({
+//     url:'https://pokeapi.co/api/v2/pokemon/172',
+//     method:'DELETE',
+//     headers:{'Content-Type':'application/json'}
+//   });
+
+//   console.log( monster );
+
+// // monster.then((res)=>{
+// //   console.log( res );
+
+// // })
