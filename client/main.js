5b70f02866c6ccbbd8fce5bfbd20ef1348b18ea7
wnsrl7250 <wnsrl7250@naver.com>
2024-11-26 22:03:00 +0900

    learn. web component
    - Button 만들기

--- client/main.js
+++ client/main.js
@@ -1,50 +1,11 @@
-// console.log(temp.content.cloneNode(true));
+import { Button } from './components/Button/Button.js';
 
-class MyElement extends HTMLElement {
-  constructor() {
-    super();
-    this.attachShadow({ mode: 'open' });
-    this.count = 0;
-  }
+const app = document.getElementById('app');
 
-  connectedCallback() {
-    this.랜더링();
-    this.card = this.shadowRoot.querySelector('.card');
-    this.card.addEventListener('click', () => {
-      this.이벤트();
-    });
-    this.card.addEventListener('click', this.이벤트.bind(this));
-  }
-
-  이벤트() {
-    console.log(++this.count);
-  }
-
-  랜더링() {
-    this.shadowRoot.innerHTML = `
-      <style>
-         .card{
-          background-color:orange;
-        }
-      </style>
-      <div class="card">
-        <h2>Card Title</h2>
-        <slot name="me"></slot>
-        <p> description..</p>
-      </div>
-    `;
-  }
-
-  static get observedAttributes() {
-    return ['data-name'];
-  }
-
-  attributeChangedCallback(name, oldValue, newValue) {
-    if (name === 'data-name') {
-      this.랜더링();
-      console.log('리랜더링');
-    }
-  }
+function defineElements() {
+  customElements.define('custom-button', Button);
 }
 
-customElements.define('my-element', MyElement);
+defineElements();
+
+app.append(document.createElement('custom-button'));
