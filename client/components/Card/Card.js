ec74327cfbc93a4a1d05d51ec6aa2a03c2f8efc0
wnsrl7250 <wnsrl7250@naver.com>
2024-11-27 15:13:22 +0900

    learn. figma to code
    - button

--- client/components/Card/Card.js
+++ client/components/Card/Card.js
@@ -4,19 +4,49 @@ export class Card extends HTMLElement {
 
     this.attachShadow({ mode: 'open' });
 
+    this.state = {
+      showImage: this.getAttribute('show-image') || 'true',
+      title: this.getAttribute('title') || 'Card Title',
+    };
+
     this.render();
   }
 
+  static get observedAttributes() {
+    return ['show-image', 'title'];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === 'show-image') {
+      this.state.showImage = newValue;
+      this.render();
+      console.log('리랜더링');
+    }
+
+    if (name === 'title') {
+      this.state.title = newValue;
+      this.render();
+    }
+  }
+
   render() {
+    const { showImage, title } = this.state;
+
+    const figure = /* html */ `
+      <figure>
+        <img src="./assets/visual.jpg" alt="" />
+        <figcaption class="a11y-hidden">눈이 소복히 쌓인 나뭇가지</figcaption>
+      </figure>
+    `;
+
     this.shadowRoot.innerHTML = `
-    <style> @import url('./components/Card/Card.css'); </style>
+      <style>@import url('./components/Card/Card.css');</style>
       <div class="card">
-        <figure>
-          <img src="./assets/visual.jpg" alt="" />
-          <figcaption class="a11y-hidden">눈이 소복히 쌓인 나뭇가지</figcaption>
-        </figure>
-        <h2>Card Title</h2>
-        <p>description...</p>
+        ${showImage === 'true' ? figure : ''}
+        <h2>${title}</h2>
+        <p>
+          <slot name="desc">description...</slot>
+        </p>
       </div>
     `;
   }
