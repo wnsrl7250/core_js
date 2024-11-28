390a6df1c35fb6b3e1697251b397ab1f0c809ece
wnsrl7250 <wnsrl7250@naver.com>
2024-11-28 13:03:15 +0900

    learn. todoList
    - todoListe
    - todoItem
    - 데이터 모델 분리
    - service
    - gsap

--- client/main.js
+++ client/main.js
@@ -1,6 +1,8 @@
 import { Button } from './components/Button/Button.js';
 import { Card } from './components/Card/Card.js';
 import { Counter } from './components/Counter/Counter.js';
+import { TodoList } from './components/TodoList/TodoList.js';
+import { TodoItem } from './components/TodoItem/TodoItem.js';
 
 const app = document.getElementById('app');
 
@@ -8,10 +10,12 @@ function defineElements() {
   customElements.define('custom-button', Button);
   customElements.define('custom-card', Card);
   customElements.define('custom-counter', Counter);
+  customElements.define('custom-todo', TodoList);
+  customElements.define('custom-todo-item', TodoItem);
 }
 
 defineElements();
 
-const buttonElement = document.createElement('custom-button');
+const todoElement = document.createElement('custom-todo');
 
-// app.append(buttonElement);
+app.append(todoElement);
